dac68d1607dd968a5511e95925154f233df87c35
Trevor van Unen <t.unen@caesar.nl>
2023-02-22 10:59:59 +0100

    Fixed deployments of function app

--- RestAPI/Program.cs
+++ RestAPI/Program.cs
@@ -16,7 +16,7 @@ namespace RestAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsProduction())
+            if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
